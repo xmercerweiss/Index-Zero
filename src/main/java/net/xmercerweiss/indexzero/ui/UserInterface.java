e1ad8b3ed88d8552fe8ddfb8fb7bec7bff8cc97e
Xavier Mercerweiss <mercerweissx@gmail.com>
2025-10-21 23:42:26 -0600

    Added basic Lanterna test

--- src/main/java/net/xmercerweiss/indexzero/ui/UserInterface.java
+++ src/main/java/net/xmercerweiss/indexzero/ui/UserInterface.java
@@ -2,12 +2,29 @@ package net.xmercerweiss.indexzero.ui;
 
 import net.xmercerweiss.indexzero.events.*;
 
+import com.googlecode.lanterna.screen.*;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.gui2.dialogs.*;
+import com.googlecode.lanterna.terminal.*;
+
+import java.io.IOException;
+
 public class UserInterface implements EventListener
 {
+  private WindowBasedTextGUI gui = null;
 
   public UserInterface()
   {
-
+    try
+    {
+      Screen screen = new DefaultTerminalFactory().createScreen();
+      gui = new MultiWindowTextGUI(screen);
+      screen.startScreen();
+    }
+    catch (IOException e)
+    {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
@@ -16,8 +33,28 @@ public class UserInterface implements EventListener
     switch (e)
     {
       case LAUNCH_APP:
-        System.out.print("Launched UI!");
+        launch();
         break;
+    }
+  }
+
+  private void launch()
+  {
+    if (gui != null)
+    {
+      MessageDialog window = new MessageDialogBuilder()
+        .setTitle("Index Zero")
+        .setText("Lets do some money-ing!")
+        .build();
+      gui.addWindow(window);
+      try
+      {
+        gui.updateScreen();
+      }
+      catch (IOException e)
+      {
+        throw new RuntimeException(e);
+      }
     }
   }
 }
